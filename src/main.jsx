bdd8740fe7158580243b4df9f99f1aebce6a2ae8
Laharipriya-2002 <laharipriya0024@gmail.com>
2025-02-05 12:02:50 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,27 +1,31 @@
 // main.jsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';  
+import { BrowserRouter } from 'react-router-dom'; // Import BrowserRouter
+import { Provider } from 'react-redux';
+
 import App from './App';
 import App1 from './App1';
 import App3 from './app3';
-import Footer from './footer'; // Ensure Footer is correctly imported
-
-import { Provider } from 'react-redux';
+import Footer from './footer';
 import { store } from './store';
+
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
-    <div className="container">
-      <div className="row">
-        <App />
-        <App1 />
+    <BrowserRouter basename="/my-react-app"> {/* Update this with your repo name */}
+      <div className="container">
+        <div className="row">
+          <App />
+          <App1 />
+        </div>
+        <div className="row">
+          <App3 />
+        </div>
+        <Footer />
       </div>
-      <div className="row">
-        <App3 />
-      </div>
-      <Footer /> {/* Footer component */}
-    </div>
+    </BrowserRouter>
   </Provider>
 );
